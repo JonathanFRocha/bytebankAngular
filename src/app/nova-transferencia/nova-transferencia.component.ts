6e77fd9f976acbf6a0d37db4ccc85df3b4405bd5
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-05-29 00:01:19 -0300

    ng module on first page implemented

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class novaTransferenciaComponent {}
+export class novaTransferenciaComponent {
+  public valor!: number;
+  public destino!: string;
+  constructor() {}
+
+  transferir() {
+    console.log('solicitado nova transferencia');
+    console.log('valor: ' + this.valor);
+    console.log('destino' + this.destino);
+  }
+}
